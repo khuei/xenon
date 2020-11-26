213e4f908f61551a26be0fc01894516fd3d54613
Khue Nguyen <Z5483Y@gmail.com>
2020-11-25 22:51:42 -0500

    style(clang-format): actually remove ColumnLimit

--- src/main.cpp
+++ src/main.cpp
@@ -20,17 +20,30 @@ opcontrol(void)
 {
 	for (;;) {
 		if (!controller.getDigital(okapi::ControllerDigital::L2)) {
-			drive->xArcade(controller.getAnalog(okapi::ControllerAnalog::leftX),
-				       controller.getAnalog(okapi::ControllerAnalog::leftY),
-				       controller.getAnalog(okapi::ControllerAnalog::rightX));
-		} else if (controller.getDigital(okapi::ControllerDigital::L2)) {
+			drive->xArcade(
+				controller.getAnalog(
+					okapi::ControllerAnalog::leftX),
+				controller.getAnalog(
+					okapi::ControllerAnalog::leftY),
+				controller.getAnalog(
+					okapi::ControllerAnalog::rightX));
+		} else if (controller.getDigital(
+				   okapi::ControllerDigital::L2)) {
 			/* move left diagonally */
-			front_right.moveVelocity(-100 * controller.getAnalog(okapi::ControllerAnalog::leftY));
-			back_left.moveVelocity(-100 * controller.getAnalog(okapi::ControllerAnalog::leftY));
+			front_right.moveVelocity(
+				-100 * controller.getAnalog(
+					       okapi::ControllerAnalog::leftY));
+			back_left.moveVelocity(
+				-100 * controller.getAnalog(
+					       okapi::ControllerAnalog::leftY));
 
 			/* move right diagonally */
-			front_left.moveVelocity(100 * controller.getAnalog(okapi::ControllerAnalog::rightY));
-			back_right.moveVelocity(100 * controller.getAnalog(okapi::ControllerAnalog::rightY));
+			front_left.moveVelocity(
+				100 * controller.getAnalog(
+					      okapi::ControllerAnalog::rightY));
+			back_right.moveVelocity(
+				100 * controller.getAnalog(
+					      okapi::ControllerAnalog::rightY));
 		}
 	}
 }
