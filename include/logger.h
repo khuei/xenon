e156c2ddd187a642ccadbe2cef6d3f3f235cf1f2
Khue Nguyen <Z5483Y@gmail.com>
2020-12-17 22:55:29 -0500

    feat(logger): add load buffer and logging function

---
+++ include/logger.h
@@ -0,0 +1,11 @@
+#ifndef LOGGER
+#define LOGGER
+
+namespace logger {
+
+char *ebuf(void);
+void elog(const char *);
+
+} // namespace logger
+
+#endif
---
+++ src/logger.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "logger.h"
+
+namespace logger {
+
+char *
+ebuf(void)
+{
+	FILE *stream;
+	double length;
+	char *buffer;
+
+	stream = fopen("/usd/event.log", "r");
+
+	if (stream) {
+		fseek(stream, 0L, SEEK_END);
+		length = ftell(stream);
+		rewind(stream);
+		buffer = (char *)malloc(length);
+
+		if (buffer)
+			fread(buffer, 1, length, stream);
+
+		fclose(stream);
+	}
+
+	return buffer;
+}
+
+void
+elog(const char *str)
+{
+	FILE *event_log = fopen("/usd/event.log", "a");
+	fprintf(event_log, "%s\n", str);
+	fclose(event_log);
+}
+
+} // namespace logger
