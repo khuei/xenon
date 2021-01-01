8fd67126849efadf3e88211827ed3d29447a0808
Khue Nguyen <Z5483Y@gmail.com>
2021-01-01 14:57:33 -0500

    feat(gui): add intake tab

--- src/gui.cpp
+++ src/gui.cpp
@@ -9,6 +9,7 @@ namespace gui {
 
 enum gui_tab {
 	CHASSIS_TAB,
+	INTAKE_TAB,
 	DIAGNOSTIC_TAB,
 	ODOM_TAB,
 	LOG_TAB,
@@ -23,6 +24,12 @@ lv_obj_t *chassis_left_speed_gauge;
 lv_obj_t *chassis_right_speed_gauge;
 lv_obj_t *chassis_left_label;
 lv_obj_t *chassis_right_label;
+
+lv_obj_t *intake_tab;
+lv_obj_t *intake_front_gauge;
+lv_obj_t *intake_internal_gauge;
+lv_obj_t *intake_front_label;
+lv_obj_t *intake_internal_label;
 
 lv_obj_t *diagnostic_tab;
 lv_obj_t *diagnostic_label;
@@ -57,6 +64,20 @@ gui_task(void)
 
 		lv_label_set_text(chassis_left_label, chassis_left_speed_str.c_str());
 		lv_label_set_text(chassis_right_label, chassis_right_speed_str.c_str());
+
+		double intake_front_speed = (left_intake->getActualVelocity() + right_intake->getActualVelocity()) / 2;
+		double intake_internal_speed = (top_intake->getActualVelocity() + bottom_intake->getActualVelocity()) / 2;
+
+		double intake_front_pos = (left_intake->getPosition() + right_intake->getPosition()) / 2;
+		double intake_internal_pos = (top_intake->getPosition() + bottom_intake->getPosition()) / 2;
+
+		std::string intake_front_str =
+			std::to_string(intake_front_speed) + " RPM\n" + std::to_string(intake_front_pos) + " DEG\n";
+		std::string intake_internal_str =
+			std::to_string(intake_internal_speed) + " RPM\n" + std::to_string(intake_internal_pos) + " DEG\n";
+
+		lv_label_set_text(intake_front_label, intake_front_str.c_str());
+		lv_label_set_text(intake_internal_label, intake_internal_str.c_str());
 
 		std::string text = "front left motor: " + std::to_string(front_left->getPosition()) + "\n" +
 				   "front right motor: " + std::to_string(front_right->getPosition()) + "\n" +
@@ -98,6 +119,9 @@ switch_tab(pros::controller_digital_e_t left, pros::controller_digital_e_t right
 	case CHASSIS_TAB:
 		lv_tabview_set_tab_act(tabview, CHASSIS_TAB, false);
 		break;
+	case INTAKE_TAB:
+		lv_tabview_set_tab_act(tabview, INTAKE_TAB, false);
+		break;
 	case DIAGNOSTIC_TAB:
 		lv_tabview_set_tab_act(tabview, DIAGNOSTIC_TAB, false);
 		break;
@@ -128,6 +152,22 @@ init(void)
 	lv_obj_align(chassis_right_speed_gauge, NULL, LV_ALIGN_IN_RIGHT_MID, -20, 0);
 	chassis_right_label = lv_label_create(chassis_tab, NULL);
 	lv_obj_align(chassis_right_label, NULL, LV_ALIGN_CENTER, 90, 50);
+
+	intake_tab = lv_tabview_add_tab(tabview, "Intake");
+
+	intake_front_gauge = lv_gauge_create(intake_tab, NULL);
+	lv_gauge_set_range(intake_front_gauge, 0, 200);
+	lv_obj_set_size(intake_front_gauge, 180, 150);
+	lv_obj_align(intake_front_gauge, NULL, LV_ALIGN_IN_LEFT_MID, 20, 0);
+	intake_front_label = lv_label_create(intake_tab, NULL);
+	lv_obj_align(intake_front_label, NULL, LV_ALIGN_CENTER, -150, 50);
+
+	intake_internal_gauge = lv_gauge_create(intake_tab, NULL);
+	lv_gauge_set_range(intake_internal_gauge, 0, 200);
+	lv_obj_set_size(intake_internal_gauge, 180, 150);
+	lv_obj_align(intake_internal_gauge, NULL, LV_ALIGN_IN_RIGHT_MID, -20, 0);
+	intake_internal_label = lv_label_create(intake_tab, NULL);
+	lv_obj_align(intake_internal_label, NULL, LV_ALIGN_CENTER, 90, 50);
 
 	diagnostic_tab = lv_tabview_add_tab(tabview, "Diagnostic");
 	diagnostic_label = lv_label_create(diagnostic_tab, NULL);
