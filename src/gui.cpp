51a86be275bd84824fd8d0c7cebe3b75eb7f484a
Khue Nguyen <Z5483Y@gmail.com>
2021-01-02 12:46:13 -0500

    feat(gui): add speed chart for chassis and intake (#27)

--- src/gui.cpp
+++ src/gui.cpp
@@ -19,7 +19,9 @@ enum theme {
 
 enum tab {
 	CHASSIS_TAB,
+	CHASSIS_GRAPH_TAB,
 	INTAKE_TAB,
+	INTAKE_GRAPH_TAB,
 	ODOM_TAB,
 	HEALTH_TAB,
 	LOG_TAB,
@@ -36,11 +38,24 @@ lv_obj_t *chassis_right_speed_gauge;
 lv_obj_t *chassis_left_label;
 lv_obj_t *chassis_right_label;
 
+lv_obj_t *chassis_graph_tab;
+lv_obj_t *chassis_chart;
+lv_obj_t *chassis_chart_label;
+lv_chart_series_t *chassis_max_speed;
+lv_chart_series_t *chassis_current_speed;
+
 lv_obj_t *intake_tab;
 lv_obj_t *intake_front_gauge;
 lv_obj_t *intake_internal_gauge;
 lv_obj_t *intake_front_label;
 lv_obj_t *intake_internal_label;
+
+lv_obj_t *intake_graph_tab;
+lv_obj_t *intake_chart;
+lv_obj_t *intake_chart_label;
+lv_chart_series_t *intake_max_speed;
+lv_chart_series_t *intake_front_current_speed;
+lv_chart_series_t *intake_internal_current_speed;
 
 lv_obj_t *health_tab;
 lv_obj_t *chassis_health_label;
@@ -63,8 +78,10 @@ gui_task(void)
 	for (;;) {
 		/*            CHASSIS TAB            */
 
-		double chassis_left_speed = (front_left->getActualVelocity() + back_left->getActualVelocity()) / 2;
-		double chassis_right_speed = (front_right->getActualVelocity() + back_right->getActualVelocity()) / 2;
+		double chassis_left_speed =
+			(fabs(front_left->getActualVelocity()) + fabs(back_left->getActualVelocity())) / 2;
+		double chassis_right_speed =
+			(fabs(front_right->getActualVelocity()) + fabs(back_right->getActualVelocity())) / 2;
 
 		double chassis_left_pos = (front_left->getPosition() + back_left->getPosition()) / 2;
 		double chassis_right_pos = (front_right->getPosition() + back_right->getPosition()) / 2;
@@ -80,11 +97,19 @@ gui_task(void)
 		lv_label_set_text(chassis_left_label, chassis_left_speed_str.c_str());
 		lv_label_set_text(chassis_right_label, chassis_right_speed_str.c_str());
 
+		/*            CHASSIS GRAPH TAB            */
+
+		lv_chart_set_next(chassis_chart, chassis_max_speed, 200);
+		lv_chart_set_next(chassis_chart,
+				  chassis_current_speed,
+				  fabs((chassis_left_speed + chassis_right_speed) / 2));
+
 		/*            INTAKE TAB            */
 
-		double intake_front_speed = (left_intake->getActualVelocity() + right_intake->getActualVelocity()) / 2;
+		double intake_front_speed =
+			(fabs(left_intake->getActualVelocity()) + fabs(right_intake->getActualVelocity())) / 2;
 		double intake_internal_speed =
-			(top_intake->getActualVelocity() + bottom_intake->getActualVelocity()) / 2;
+			(fabs(top_intake->getActualVelocity()) + fabs(bottom_intake->getActualVelocity())) / 2;
 
 		lv_gauge_set_value(intake_front_gauge, 0, fabs(intake_front_speed));
 		lv_gauge_set_value(intake_internal_gauge, 0, fabs(intake_internal_speed));
@@ -100,6 +125,12 @@ gui_task(void)
 		lv_label_set_text(intake_front_label, intake_front_str.c_str());
 		lv_label_set_text(intake_internal_label, intake_internal_str.c_str());
 
+		/*            INTAKE GRAPH TAB            */
+
+		lv_chart_set_next(intake_chart, intake_max_speed, 200);
+		lv_chart_set_next(intake_chart, intake_front_current_speed, intake_front_speed);
+		lv_chart_set_next(intake_chart, intake_internal_current_speed, intake_internal_speed);
+
 		/*            ODOM TAB            */
 
 		std::string pos = "global_x: " + std::to_string(drive->getState().x.convert(okapi::inch)) + "_in\n" +
@@ -110,23 +141,23 @@ gui_task(void)
 
 		/*            HEALTH TAB            */
 
-		std::string chassis_health_str = "Chassis\n\nfront left: " + checkhealth::motor(front_left) +
-						 "\n" + "front right: " + checkhealth::motor(front_right) +
-						 "\n" + "back left: " + checkhealth::motor(back_left) +
-						 "\n" + "back right: " + checkhealth::motor(back_right);
+		std::string chassis_health_str = "Chassis\n\nfront left: " + checkhealth::motor(front_left) + "\n" +
+						 "front right: " + checkhealth::motor(front_right) + "\n" +
+						 "back left: " + checkhealth::motor(back_left) + "\n" +
+						 "back right: " + checkhealth::motor(back_right);
 
 		lv_label_set_text(chassis_health_label, chassis_health_str.c_str());
 
-		std::string intake_health_str = "Intake\n\nleft: " + checkhealth::motor(left_intake) +
-						 "\n" + "right: " + checkhealth::motor(right_intake) +
-						 "\n" + "top: " + checkhealth::motor(top_intake) +
-						 "\n" + "bot: " + checkhealth::motor(bottom_intake);
+		std::string intake_health_str = "Intake\n\nleft: " + checkhealth::motor(left_intake) + "\n" +
+						"right: " + checkhealth::motor(right_intake) + "\n" +
+						"top: " + checkhealth::motor(top_intake) + "\n" +
+						"bot: " + checkhealth::motor(bottom_intake);
 
 		lv_label_set_text(intake_health_label, intake_health_str.c_str());
 
-		std::string core_health_str = "Core\n\ncontroller: " + checkhealth::controller(master) + "\n"
-			+ "battery: " + checkhealth::battery() + "\n"
-			+ "usd: " + checkhealth::usd();
+		std::string core_health_str = "Core\n\ncontroller: " + checkhealth::controller(master) + "\n" +
+					      "battery: " + checkhealth::battery() + "\n" +
+					      "usd: " + checkhealth::usd();
 
 		lv_label_set_text(core_health_label, core_health_str.c_str());
 
@@ -134,7 +165,7 @@ gui_task(void)
 
 		lv_label_set_text(log_stream, logger::ebuf());
 
-		pros::delay(1000);
+		pros::delay(100);
 	}
 }
 
@@ -159,8 +190,14 @@ switch_tab(pros::controller_digital_e_t left, pros::controller_digital_e_t right
 	case CHASSIS_TAB:
 		lv_tabview_set_tab_act(tabview, CHASSIS_TAB, false);
 		break;
+	case CHASSIS_GRAPH_TAB:
+		lv_tabview_set_tab_act(tabview, CHASSIS_GRAPH_TAB, false);
+		break;
 	case INTAKE_TAB:
 		lv_tabview_set_tab_act(tabview, INTAKE_TAB, false);
+		break;
+	case INTAKE_GRAPH_TAB:
+		lv_tabview_set_tab_act(tabview, INTAKE_GRAPH_TAB, false);
 		break;
 	case ODOM_TAB:
 		lv_tabview_set_tab_act(tabview, ODOM_TAB, false);
@@ -240,6 +277,30 @@ init(void)
 
 	logger::elog("gui: create chassis tab");
 
+	/*            CHASSIS GRAPH TAB            */
+
+	chassis_graph_tab = lv_tabview_add_tab(tabview, "graph");
+
+	chassis_chart = lv_chart_create(chassis_graph_tab, NULL);
+	lv_chart_set_type(chassis_chart, LV_CHART_TYPE_LINE);
+	lv_chart_set_range(chassis_chart, 0, 250);
+	lv_chart_set_series_width(chassis_chart, 2);
+	lv_chart_set_div_line_count(chassis_chart, 10, 20);
+	lv_obj_align(chassis_chart, NULL, LV_ALIGN_OUT_LEFT_TOP, 0, 0);
+	lv_obj_set_size(chassis_chart, 700, 140);
+
+	chassis_max_speed = lv_chart_add_series(chassis_chart, LV_COLOR_RED);
+	chassis_current_speed = lv_chart_add_series(chassis_chart, LV_COLOR_LIME);
+
+	chassis_chart_label = lv_label_create(chassis_chart, NULL);
+	lv_label_set_recolor(chassis_chart_label, true);
+	lv_label_set_text(chassis_chart_label,
+			  "#ff0000 max_speed#\n"
+			  "#00ff00 current_speed#\n");
+	lv_obj_align(chassis_chart_label, NULL, LV_ALIGN_CENTER, -120, 50);
+
+	logger::elog("gui: create chassis graph");
+
 	/*            INTAKE TAB            */
 
 	intake_tab = lv_tabview_add_tab(tabview, "intake");
@@ -259,6 +320,32 @@ init(void)
 	lv_obj_align(intake_internal_label, NULL, LV_ALIGN_CENTER, 90, 50);
 
 	logger::elog("gui: create intake tab");
+
+	/*            INTAKE GRAPH TAB            */
+
+	intake_graph_tab = lv_tabview_add_tab(tabview, "graph");
+
+	intake_chart = lv_chart_create(intake_graph_tab, NULL);
+	lv_chart_set_type(intake_chart, LV_CHART_TYPE_LINE);
+	lv_chart_set_range(intake_chart, 0, 250);
+	lv_chart_set_series_width(intake_chart, 2);
+	lv_chart_set_div_line_count(intake_chart, 10, 20);
+	lv_obj_align(intake_chart, NULL, LV_ALIGN_OUT_LEFT_TOP, 0, 0);
+	lv_obj_set_size(intake_chart, 700, 140);
+
+	intake_max_speed = lv_chart_add_series(intake_chart, LV_COLOR_RED);
+	intake_front_current_speed = lv_chart_add_series(intake_chart, LV_COLOR_LIME);
+	intake_internal_current_speed = lv_chart_add_series(intake_chart, LV_COLOR_CYAN);
+
+	intake_chart_label = lv_label_create(intake_chart, NULL);
+	lv_label_set_recolor(intake_chart_label, true);
+	lv_label_set_text(intake_chart_label,
+			  "#ff0000 max_speed#\n"
+			  "#00ff00 front_current_speed#\n"
+			  "#00ffff internal_current_speed\n");
+	lv_obj_align(intake_chart_label, NULL, LV_ALIGN_CENTER, -90, 40);
+
+	logger::elog("gui: create intake graph");
 
 	/*            ODOM TAB            */
 
